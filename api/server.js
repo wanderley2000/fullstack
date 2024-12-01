a796e67bb8f7024025857c4cd472965510ac839c
wanderley2000 <w.saldarriaga14@ciaf.edu.co>
2024-12-01 01:39:57 -0500

    Actualizacion-5

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://fullstack-chi-lovat.vercel.app'],
+    origin: ['http://localhost:3000', 'fullstack-woad-iota.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }));
